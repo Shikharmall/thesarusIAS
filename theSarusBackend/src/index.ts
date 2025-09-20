0450f064ddf2a3784c433937e43cdc34cf83dcc6
Shikhar Mall <shmall.21.2020@gmail.com>
2025-09-20 14:02:12 +0530

    added

--- theSarusBackend/src/index.ts
+++ theSarusBackend/src/index.ts
@@ -1,12 +1,14 @@
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config();
+
 import express from "express";
 import type { Application } from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import dotenv from "dotenv";
 import mongoose from "mongoose";
 import indexRouter from "./routes/indexRouter";
 
-dotenv.config();
 
 const app: Application = express();
 const port: number = Number(process.env.PORT) || 5174;
@@ -32,7 +34,7 @@ app.use(
   })
 );
 
-app.use("/api/", indexRouter);
+app.use("/api/v1/", indexRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
